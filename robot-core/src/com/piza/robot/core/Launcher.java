9bd2b5a21a322e669c0e8e81fb65ad11c422ef98
Peter <wenbin.cao@ktbhealth.com>
2016-09-26 20:00:19 +0800

    hello world

---
+++ robot-core/src/com/piza/robot/core/Launcher.java
@@ -0,0 +1,11 @@
+package com.piza.robot.core;
+
+/**
+ * Created by Peter on 16/9/26.
+ */
+public class Launcher {
+
+    public static void main(String[] args) {
+        System.out.println("hello world! "+Version.VERSION);
+    }
+}
---
+++ robot-deployer/src/com/piza/robot/deployer/DeployerVersion.java
@@ -0,0 +1,10 @@
+package com.piza.robot.deployer;
+
+/**
+ * Created by Peter on 16/9/26.
+ */
+public class DeployerVersion {
+
+    public static final String DEPLOYER_VERSION="1.0.0";
+
+}
