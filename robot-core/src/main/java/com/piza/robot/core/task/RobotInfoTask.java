7dcd287a06a82ea62491ee3bcb685aebfe3502b0
Peter <wenbin.cao@ktbhealth.com>
2016-10-03 19:15:02 +0800

    show memory

--- robot-core/src/main/java/com/piza/robot/core/task/RobotInfoTask.java
+++ robot-core/src/main/java/com/piza/robot/core/task/RobotInfoTask.java
@@ -14,10 +14,16 @@ public class RobotInfoTask  extends TaskBase {
     @Override
     public void run() {
         String sendMsg="status report:\n"
+                +getSystemInfo()
                 +ParserManage.getInstance().report()
                 +TaskManager.getInstance().report()
                 +TaskService.getInstance().report()+"\n"
                 +ChatService.getInstance().report();
         ChatService.getInstance().sendMessage(this.chatMessage.getFriend(),sendMsg);
+    }
+
+    private String getSystemInfo(){
+        return "freeMemory:"+Runtime.getRuntime().freeMemory()+",maxMemory:"+Runtime.getRuntime().maxMemory()
+                +"totalMemory:"+Runtime.getRuntime().totalMemory()+"\n";
     }
 }
