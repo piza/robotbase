4318b91fc07554a3b99c2493cc50de6f64a0d0fe
Peter <wenbin.cao@ktbhealth.com>
2016-10-08 11:51:53 +0800

    deploy task

--- robot-deployer/src/main/java/com/piza/robot/deployer/DeployTask.java
+++ robot-deployer/src/main/java/com/piza/robot/deployer/DeployTask.java
@@ -1,13 +1,17 @@
 package com.piza.robot.deployer;
 
-import com.piza.robot.core.ChatService;
-import com.piza.robot.core.TaskBase;
+import com.piza.robot.core.*;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by Peter on 16/9/28.
  */
 public class DeployTask extends TaskBase {
 
+    private volatile static boolean working=false;
 
     @Override
     public String getTaskName() {
@@ -17,17 +21,145 @@ public class DeployTask extends TaskBase {
     @Override
     public void run() {
 
-        String sendMsg="start task:"+this.chatMessage.getContent();
-        ChatService.getInstance().sendMessage(this.chatMessage.getFriend(),sendMsg);
+        if(working){
+            this.sendChat("duplicate command!");
+            return;
+        }
+        try {
+            working=true;
+            this.sendChat("ok,start deploy task!\n pull code...");
+            if(!pullCode()){
+                this.sendChat("task over");
+                return;
+            }
+
+            if(!buildProject()){
+                this.sendChat("task over");
+                return;
+            }
+            if(!deployProject()){
+                this.sendChat("task over");
+                return;
+            }
+            if(!restartTomcat()){
+                this.sendChat("task over");
+                return;
+            }
+        }finally {
+            working=false;
+        }
+    }
+    private boolean deployProject(){
+
+        this.sendChat("start to deploy project!");
+        String workingDir= ConfigUtil.getStrProp("workDir");
 
         try {
-            Thread.sleep(12000);
-        } catch (InterruptedException e) {
+            String pullCmd = workingDir + File.separator + "deployPocketmoney.sh "+ConfigUtil.getStrProp("pocketmoneyDir")+" "+ConfigUtil.getStrProp("deployDir");
+            ShellJob shellJob=new ShellJob();
+            shellJob.runCommand(pullCmd);
+            this.sendChat("["+shellJob.isSuccess()+"]"+shellJob.getResult());
+            return shellJob.isSuccess();
+        }catch (Exception e){
             e.printStackTrace();
+            this.sendChat("error when deploy project:" + e.getMessage());
         }
 
-         sendMsg="end task of deploy";
-        ChatService.getInstance().sendMessage(this.chatMessage.getFriend(),sendMsg);
+        return true;
+    }
+    private boolean restartTomcat(){
 
+        this.sendChat("start to restart tomcat!");
+        String workingDir= ConfigUtil.getStrProp("workDir");
+
+        try {
+            String pullCmd = workingDir + File.separator + "restartTomcat.sh "+ConfigUtil.getStrProp("tomcatDir");
+            ShellJob shellJob=new ShellJob();
+            shellJob.runCommand(pullCmd);
+            this.sendChat("["+shellJob.isSuccess()+"]"+shellJob.getResult());
+            return shellJob.isSuccess();
+        }catch (Exception e){
+            e.printStackTrace();
+            this.sendChat("error when restart tomcat:" + e.getMessage());
+        }
+
+        return true;
+    }
+    private boolean buildProject(){
+        this.sendChat("start to build project!");
+        String workingDir= ConfigUtil.getStrProp("workDir");
+
+        try {
+            String pullCmd = workingDir + File.separator + "buildProject.sh "+workingDir+File.separator+"robotbase";
+            ShellJob shellJob=new ShellJob();
+            shellJob.runCommand(pullCmd);
+            this.sendChat("["+shellJob.isSuccess()+"]"+shellJob.getResult());
+            return shellJob.isSuccess();
+        }catch (Exception e){
+            e.printStackTrace();
+            this.sendChat("error when build project:" + e.getMessage());
+        }
+        return false;
+    }
+    private boolean pullCode(){
+        if(!checkFirst()){
+           return false;
+        }
+        this.sendChat("start to pull code!");
+        String workingDir= ConfigUtil.getStrProp("workDir");
+
+        try {
+            String pullCmd = workingDir + File.separator + "pullProject.sh "+ConfigUtil.getStrProp("pocketmoneyDir");
+            ShellJob shellJob=new ShellJob();
+            shellJob.runCommand(pullCmd);
+            this.sendChat("["+shellJob.isSuccess()+"]"+shellJob.getResult());
+            if(shellJob.getResult()!=null && shellJob.getResult().contains("Already up-to-date")){
+                this.sendChat("no code changed, cancel task!");
+                return false;
+            }
+            return shellJob.isSuccess();
+        }catch (Exception e){
+            e.printStackTrace();
+            this.sendChat("error when pull code:" + e.getMessage());
+        }
+        return false;
+    }
+
+    private boolean checkFirst(){
+        String workingDir= ConfigUtil.getStrProp("workDir");
+
+        File robotDir=new File(workingDir);
+        if(!robotDir.exists()) {
+            robotDir.mkdir();
+        }
+
+        checkShellFile(workingDir,"shells/pullProject.sh");
+        checkShellFile(workingDir,"shells/buildProject.sh");
+        checkShellFile(workingDir,"shells_deployer/deployPocketmoney.sh");
+        checkShellFile(workingDir,"shells_deployer/restartTomcat.sh");
+
+        String projectDir=ConfigUtil.getStrProp("pocketmoneyDir");
+
+        File projectFolder=new File(projectDir);
+        if(projectFolder.exists()){
+            return true;
+        }else{
+            this.sendChat("no src dir, pls clone it !");
+            return false;
+        }
+    }
+
+
+    private void checkShellFile(String workingDir,String shellName){
+        File shellFile=new File(workingDir+File.separator+shellName);
+        if(!shellFile.exists()) {
+            try {
+                this.sendChat("copy "+shellName);
+                FileUtils.copyInputStreamToFile(DeployTask.class.getClassLoader().getResourceAsStream( shellName), shellFile);
+                shellFile.setExecutable(true);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
