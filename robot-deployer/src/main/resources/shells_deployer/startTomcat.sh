e51192836fd7a9beed4dde11b2e7923fa3eb909b
Peter <wenbin.cao@ktbhealth.com>
2016-10-08 13:44:01 +0800

    fix bug

--- robot-deployer/src/main/resources/shells_deployer/startTomcat.sh
+++ robot-deployer/src/main/resources/shells_deployer/startTomcat.sh
@@ -5,7 +5,11 @@ tomcartDir=$1
 echo "tomcat dir:"
 echo $tomcartDir
 
-#ps -ef | grep tomcat | grep -v grep | awk {'print $2'} | xargs kill -9
-
+pid=`ps -ef | grep tomcat | grep -v grep | awk {'print $2'}`
+if [ "${pid}" != "" ]
+then
+echo "force shutdown tomcat"
+kill -9 $pid
+fi
 echo "start up tomcat"
 $tomcartDir/bin/startup.sh
