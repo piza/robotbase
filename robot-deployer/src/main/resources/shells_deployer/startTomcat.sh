ac5ed0b2cfe65c4ee1e73e4ac923e99cd9c5a97f
Peter <wenbin.cao@ktbhealth.com>
2016-10-08 13:31:26 +0800

    fix bug

--- robot-deployer/src/main/resources/shells_deployer/startTomcat.sh
+++ robot-deployer/src/main/resources/shells_deployer/startTomcat.sh
@@ -5,7 +5,7 @@ tomcartDir=$1
 echo "tomcat dir:"
 echo $tomcartDir
 
-ps -ef | grep tomcat | grep -v grep | awk {'print $2'} | xargs kill -9
+#ps -ef | grep tomcat | grep -v grep | awk {'print $2'} | xargs kill -9
 
 echo "start up tomcat"
 $tomcartDir/bin/startup.sh
