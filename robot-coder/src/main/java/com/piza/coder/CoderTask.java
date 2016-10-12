e8c9475dfbc76e47bc7adf8620714ecd410a7fea
Peter <wenbin.cao@ktbhealth.com>
2016-10-12 10:29:41 +0800

    fix bug

--- robot-coder/src/main/java/com/piza/coder/CoderTask.java
+++ robot-coder/src/main/java/com/piza/coder/CoderTask.java
@@ -149,7 +149,7 @@ public class CoderTask extends TaskBase {
             JavaClientGeneratorConfiguration javaClientGeneratorConfiguration=new JavaClientGeneratorConfiguration();
             javaClientGeneratorConfiguration.setTargetPackage(ConfigUtil.getStrProp("coder.basePackage") + ".dao");
             javaClientGeneratorConfiguration.setTargetProject(ConfigUtil.getStrProp("coder.ormPath") + "/java/");
-            javaClientGeneratorConfiguration.addProperty("type","XMLMAPPER");
+            javaClientGeneratorConfiguration.setConfigurationType("XMLMAPPER");
             context.setJavaClientGeneratorConfiguration(javaClientGeneratorConfiguration);
 
             SqlMapGeneratorConfiguration sqlMapGeneratorConfiguration=new SqlMapGeneratorConfiguration();
