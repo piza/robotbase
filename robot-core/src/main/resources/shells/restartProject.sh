23023ef6c17d3b1deec892b4660ba23bf62b458d
Peter <wenbin.cao@ktbhealth.com>
2016-12-09 09:45:22 +0800

    change log dir

--- robot-core/src/main/resources/shells/restartProject.sh
+++ robot-core/src/main/resources/shells/restartProject.sh
@@ -6,4 +6,4 @@ ps -ef | grep "robot-deployer" | grep -v grep | awk {'print $2'} | xargs kill -9
 
 echo 'start project'
 cd robot-deployer/target
-nohup java -jar ./robot-deployer-1.0.jar >> robot.log &
+nohup java -jar ./robot-deployer-1.0.jar >> /root/robot/logs/robot.log &
